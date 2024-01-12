fce9ce3b37b7d7cd0b4c721d05d9fea8c500c951
ibrahim bayburtlu <ibrahimbayburtlu5gmail.com>
2024-01-12 23:29:59 +0300

    Eureka Server

--- discovery-service/src/main/java/com/ibrahimbayburtlu/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/ibrahimbayburtlu/discoveryservice/DiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.ibrahimbayburtlu.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Eureka Server'ı aktif etmek için bu anotation kullanılmalıdır.
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/ibrahimbayburtlu/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ibrahimbayburtlu/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ public class OrderService {
 
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -38,12 +38,12 @@ public class OrderService {
                 .toList();
         // Call Inventory Service, and place order if product is in
         // stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                 .uri("http://localhost:8082/api/inventory",
-                         uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                 .retrieve()
-                 .bodyToMono(InventoryResponse[].class)
-                 .block();
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
 
         assert inventoryResponsesArray != null;
         boolean allProductInStock = Arrays.stream(inventoryResponsesArray)
